e221732f4c0dce8f58674ab681f32d7577202ff6
Kibbsy <ojenikedavid@gmail.com>
2024-02-15 15:55:17 +0100

    minute addition to previous hero section codes and new js

---
+++ Javacript/tweetCounter/index.js
@@ -0,0 +1,4 @@
+tweet=prompt("Compose a tweet");
+tweetCount=tweet.length;
+tweetLeft=(240-tweetCount);
+alert("You have written " + tweetCount + " characters and you have "+ tweetLeft + " characters left.")
